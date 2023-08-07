47e49ec6a1572bbb2ec1747ddba789cd5ec0b9a9
christopher Sebastian <christophersebastian@christophersmbp.myfiosgateway.com>
2023-08-06 23:12:27 -0400

    more web set up

--- index.ts
+++ index.ts
@@ -4,13 +4,33 @@ const port = 8000;
 const app = express();
 
 app.set('view engine','ejs');
+app.use(express.static("public"))
 
 app.get("/",(req,res)=>{
-    res.send("test1");
+    res.render("home");
 });
-app.get("/hi",(req,res)=>{
-    res.send("test2");
+
+app.get("/about",(req,res)=>{
+    res.render("about");
 });
+
+app.get("/loading",(req,res)=>{
+    res.render("loading");
+});
+
+app.get("/images",(req,res)=>{
+    res.render("finalpage");
+});
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+app.get("/configure",(req,res)=>{
+    res.render("options");
+});
+
+
 
 app.listen(port, () => {
     console.log(`now listening on port ${port}`);
