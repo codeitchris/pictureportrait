7272dcbc8b3447324001b0cbd89979cf78e58f54
christopher Sebastian <christophersebastian@christophersmbp.myfiosgateway.com>
2023-08-08 22:54:49 -0400

    start of the spot API stuff

--- index.ts
+++ index.ts
@@ -1,14 +1,35 @@
 import express, {Express, Request, Response} from "express";
 import * as dotenv from 'dotenv';
-const port = 8000;
+import request from 'request'; // "Request" library
+import cors from 'cors';
+import querystring from 'querystring';
+import cookieParser from 'cookie-parser';
 
+
+const port = 8000;
 const app = express();
+
 
 dotenv.config();
 
+// following from the official spotify example here: https://github.com/spotify/web-api-examples/blob/master/authentication/authorization_code/app.js 
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const redirect_uri = "/options";
+const stateKey = "idk-rn";
+
+function generateRandomString(length: number) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
 
 app.set('view engine','ejs');
-app.use(express.static("public"))
+app.use(express.static("public")).use(cors()).use(cookieParser())
 
 app.get("/",(req,res)=>{
     res.render("home");
@@ -27,7 +48,21 @@ app.get("/images",(req,res)=>{
 });
 
 app.get("/login",(req,res)=>{
-    res.render("login");
+
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  //your application requests authorization
+  var scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+    
 });
 
 app.get("/configure",(req,res)=>{
