20d2cf85406af7b040b5a076b5a81d1d7ea2537b
christopher Sebastian <cpsebastian7@gmail.com>
2023-08-11 23:20:00 -0400

    set up mongo

--- getTopTracks.ts
+++ getTopTracks.ts
@@ -1,3 +1,12 @@
+import {MongoClient} from "mongodb";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const usr = process.env.MONGO_USR;
+const pas = process.env.MONGO_PAS;
+
+
+
 async function fetchWebApi(endpoint: string, method:string, token: string) {
     const res = await fetch(`https://api.spotify.com/${endpoint}`, {
       headers: {
@@ -14,7 +23,49 @@ async function getTopTracks(token:string){
     'v1/me/top/tracks?time_range=short_term&limit=5', 'GET', token)).items;
 }
 
-export function runit(token: string){
-  const topTracks = getTopTracks(token);
+
+
+async function getToken(client: MongoClient, artist: string): Promise<string> {
+  // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#findOne for the findOne() docs
+  const result = await client.db("").collection("").findOne({ name: artist });
+
+  if (result) {
+      return result["token"];
+  } else {
+      return "";
+  }
+}
+
+export async function runit(token: string){
+  const topTracks: Object = await getTopTracks(token);
+  const uri = ``; 
   
+  const client = new MongoClient(uri);
+
+  try{
+    await client.connect();
+  } catch(e){
+    console.error(e);
+  } finally{
+    await client.close();
+  }
+  
+  var arr: Array<string> = [];
+  var notTrained: boolean = false;
+
+  for( let i =0; i<5; i++) {
+    var artist: string = Object.values(topTracks)[i]["artist"];
+    var tok: string = await getToken(client,artist);
+    if(tok == "") {
+      notTrained = true;
+      arr.push(artist);
+    } else {
+      arr.push(tok);
+    }
+  }
+
+  //now we just need to hit endpoint and push an image to a folder and then show 
+  //that on the  images tab.
+
+
 }
