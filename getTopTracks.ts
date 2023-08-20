c5b760b22572264be22f01c8602ac460b291da56
christopher Sebastian <cpsebastian7@gmail.com>
2023-08-20 16:29:38 -0400

    changing my mind

--- getTopTracks.ts
+++ getTopTracks.ts
@@ -1,13 +1,6 @@
-import {Collection, MongoClient} from "mongodb";
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-const usr = process.env.MONGO_USR;
-const pas = process.env.MONGO_PAS;
-
-interface keyable {
-  [key: string]: any  
-}
 
 async function fetchWebApi(endpoint: string, method:string, token: string) {
     const res = await fetch(`https://api.spotify.com/${endpoint}`, {
@@ -32,39 +25,35 @@ function getRandomInt(min:number, max:number) {
   return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
 
+// Call for top like 15 artists
+// Loop through and try to find at most 5 artists that have been trained on
+// If we get five, run it
+// If we don't find the most popular artists that are present in the top 15 to fill the gap
+// Run it with those names
+const artistTokenMap = new Map<string, string>();
+const styles: Array<string> = ["photo of", "picture of", "painting of", "portrait of", "image of"];
+const actions: Array<string> = ["dancing","smiling","laughing","playing pool","talking","playing cards","having cofee"];
+var notTrained: boolean = false;
 
-async function getPrompt(client: MongoClient, topTracks: Object, col: Collection): Promise<string> {
+async function getPrompt(topTracks: Object): Promise<string> {
+
+  let numStyles = getRandomInt(0,styles.length);
+  let numActions = getRandomInt(0,actions.length);
+
+  let style = styles[numStyles];
+  let action = actions[numActions];
+
   var arr: Array<string> = [];
-  var notTrained: boolean = false;
 
-  const cursor = await col.find().limit(1);
-  let style: string;
-  let art: string;
-  let action: string;
-  let tokens: string;
-
-  for await (const doc of cursor) {
-    let styles: Array<string> = doc.styles;
-    let arts: Array<string> = doc.arts;
-    let actions: Array<string> = doc.actions;
-
-    let numStyles = getRandomInt(0,styles.length);
-    let numArts = getRandomInt(0,arts.length);
-    let numActions = getRandomInt(0,actions.length);
-
-    style = styles[numStyles];
-    art = arts[numArts];
-    action = actions[numActions];
-
-    let tokens: keyable = doc.tokens;
-    for(let i =0;i<5;i++) {
+    for(let i =0;i<15;i++) {
       var artist: string = Object.values(topTracks)[i]["artist"];
-      let tok = tokens?.artist;
-      if(tok == null) {
-        notTrained = true;
-        arr.push(artist);
+
+      if(artistTokenMap.has(artist)) {
+        arr.push(artistTokenMap.get(artist)||"")
+        //useless, silly tyescript rule ugh
       } else {
-        arr.push(tok);
+        notTrained = true;
+        arr.push(artist + "(musician)");
       }
     }
 
@@ -78,32 +67,15 @@ async function getPrompt(client: MongoClient, topTracks: Object, col: Collection
       }
     }
 
-    return ("A " + style + " " + art + " " + allPeople + " " + action);
-    // hit endpoint with prompt
-  }
-  return "";
+    return ("A " + style + " " + allPeople + " " + action);
 }
 
 export async function runit(token: string){
   const topTracks: Object = await getTopTracks(token);
-  const uri = ``; 
   
-  const client = new MongoClient(uri);
-  const col = client.db("portraitBackend").collection("allData");
+  let prompt: string = await getPrompt(topTracks);
 
-  try{
-    await client.connect();
-  } catch(e){
-    console.error(e);
-  } finally{
-    await client.close();
-  }
-  
-  let prompt: string = await getPrompt(client,topTracks,col);
-  if(prompt == "") {
-    throw console.error();
-  } else {
-    // hit endpoint;
-    
-  }
+  //hit endpoint
+  // remember to deal with what happens if notTrained is true. m
+
 }
